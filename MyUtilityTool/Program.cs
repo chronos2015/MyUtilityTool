10173924c8b5cff9ac8cfc2df48b88a478312e9a
chronos2015 <tnomura217@gmail.com>
2026-04-29 17:34:29 +0900

    WindowsFormを生成するためのコードを追加

--- MyUtilityTool/Program.cs
+++ MyUtilityTool/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyUtilityTool
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
